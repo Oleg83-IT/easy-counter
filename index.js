6750f737cfd0ea453179765b3ec4a54c1c8d8a52
Oleg83-IT <olegvyacheslavovich83@gmail.com>
2022-02-21 15:12:24 +0200

    'Reset total count' and 'Choose counter's step' options are added. Assigning inner text is wrapped into a function

--- index.js
+++ index.js
@@ -2,18 +2,29 @@ const minus = document.getElementById('minus')
 
 const plus = document.getElementById('plus')
 
+const resetCounter = document.getElementById('reset-counter')
+
 const totalCount = document.getElementById('totalCount')
+
+const counterStep = document.getElementById('counter-step')
 
 let counter = 0
 
-totalCount.innerText = `Total count: ${counter}`
+const setInnerText = counterValue => totalCount.innerText = `Total count: ${counterValue}`
+
+setInnerText(counter)
 
 plus.onclick = () => {
-  counter++
-  totalCount.innerText = `Total count: ${counter}`
+  counter += +counterStep.value
+  setInnerText(counter)
 }
 
 minus.onclick = () => {
-  counter--
-  totalCount.innerText = `Total count: ${counter}`
+  counter -= +counterStep.value
+  setInnerText(counter)
+}
+
+resetCounter.onclick = () => {
+  counter = 0
+  setInnerText(counter)
 }
