8f6bcde153e40af271313494a212380c279675d9
Oleg83-IT <olegvyacheslavovich83@gmail.com>
2022-02-22 23:08:21 +0200

    Choose counter's step as slider, two new reset buttons added, design enhanced

--- index.js
+++ index.js
@@ -8,11 +8,20 @@ const totalCount = document.getElementById('totalCount')
 
 const counterStep = document.getElementById('counter-step')
 
+const resetCounterStep = document.getElementById('reset-counter-step')
+
+const resetBoth = document.getElementById('reset-both')
+
+const stepValue = document.getElementById('step-value')
+
 let counter = 0
 
-const setInnerText = counterValue => totalCount.innerText = `Total count: ${counterValue}`
+const setInnerText = 
+  counterValue => totalCount.innerText = `Total count: ${counterValue}`
 
 setInnerText(counter)
+
+stepValue.innerText = counterStep.value
 
 plus.onclick = () => {
   counter += +counterStep.value
@@ -27,4 +36,22 @@ minus.onclick = () => {
 resetCounter.onclick = () => {
   counter = 0
   setInnerText(counter)
+}
+
+resetCounterStep.onclick = () => {
+  counterStep.value = 1
+  stepValue.innerText = counterStep.value
+}
+
+resetBoth.onclick = () => {
+  resetCounter.onclick()
+  resetCounterStep.onclick()
+}
+
+counterStep.onchange = () => {
+  stepValue.innerText = counterStep.value
+}
+
+counterStep.onmousemove = () => {
+  stepValue.innerText = counterStep.value
 }
